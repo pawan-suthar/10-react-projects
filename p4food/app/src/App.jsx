1348bd6eb68bedc7b6da6b10766a03f42d56d943
pawan suthar <pawansuthar15372097023627153@gmail.com>
2023-05-20 14:18:12 +0530

    p4 header Created with global css for styled compo

--- p4food/app/src/App.jsx
+++ p4food/app/src/App.jsx
@@ -1,5 +1,44 @@
+import styled from "styled-components";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Container>
+      <Headerection>
+        <div className="log">
+          <img src="/logo.svg" alt="logo" />
+        </div>
+        <div className="search">
+          <input placeholder="Search for food" />
+        </div>
+      </Headerection>
+    </Container>
+  );
 };
 
 export default App;
+
+const Container = styled.div`
+  /* background-color: #323334; */
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const Headerection = styled.section`
+  min-height: 140px;
+  display: flex;
+  align-items: center;
+  padding: 16px;
+  justify-content: space-between;
+
+  .search {
+    input {
+      background: transparent;
+      border: 1px solid red;
+      border-radius: 5px;
+      color: white;
+      height: 40px;
+      font-size: 16px;
+      padding: 0 10px;
+    }
+  }
+`;
--- p4food/app/src/main.jsx
+++ p4food/app/src/main.jsx
@@ -2,8 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+// global style for styled components
+import { createGlobalStyle } from "styled-components";
+
+const Globalstyle = createGlobalStyle`
+
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body{
+  background-color: #323334;
+  color: white;
+}
+
+`;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Globalstyle />
     <App />
   </React.StrictMode>
 );
