cf608d03989a13cdb4449b150e07c99edde9f419
pawan suthar <pawansuthar15372097023627153@gmail.com>
2023-05-25 13:35:17 +0530

    p5 start and nav created

---
+++ p5contactbook/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="mx-auto max-w-[370px]">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
