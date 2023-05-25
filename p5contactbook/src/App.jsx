f79441cdb1646ebf3f8e626577492120d4587a78
pawan suthar <pawansuthar15372097023627153@gmail.com>
2023-05-25 13:52:41 +0530

    serch + add

--- p5contactbook/src/App.jsx
+++ p5contactbook/src/App.jsx
@@ -1,9 +1,22 @@
 import Navbar from "./components/Navbar";
+import { BiSearchAlt } from "react-icons/bi";
+import { IoMdPersonAdd } from "react-icons/io";
 
 const App = () => {
   return (
-    <div className="mx-auto max-w-[370px]">
+    <div className="mx-auto max-w-[370px]  px-4">
       <Navbar />
+      <div className="flex gap-2 ">
+        <div className=" relative flex flex-grow items-center   ">
+          <BiSearchAlt className=" absolute ml-2 text-2xl text-white" />
+          <input
+            type="text"
+            className="h-10 flex-grow rounded-md border border-white bg-transparent pl-9 text-white"
+          />
+        </div>
+
+        <IoMdPersonAdd className="mt-1 cursor-pointer items-center text-3xl text-white " />
+      </div>
     </div>
   );
 };
--- p5contactbook/src/components/Navbar.jsx
+++ p5contactbook/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="m-4 flex h-[60px] items-center justify-center gap-2 rounded-lg bg-white text-xl font-medium">
+    <div className="my-4 flex h-[60px] items-center justify-center gap-2 rounded-lg bg-white text-xl font-medium">
       <img src="/logos_firebase.png" alt="logo" />
       <h1>Firebase contact app</h1>
     </div>
